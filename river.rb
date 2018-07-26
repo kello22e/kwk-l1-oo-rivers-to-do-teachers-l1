e825b9bb1ff2ac4320443274faba2a118ea242ed
kello22e <kello22e@mtholyoke.edu>
2018-07-26 19:27:04 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,15 @@
 # river.rb
+class River
+  attr_accessor :name, :length, :countries, :discharge
+  def initialize(name)
+    @name = name
+  end
+  
+  def flood
+    self.discharge *= 1.3
+  end
+  
+  def dry_up
+    self.discharge *= 0.5
+  end
+end
